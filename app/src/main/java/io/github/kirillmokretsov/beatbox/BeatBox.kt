51d0ce3c2df6a399ba2f674ef859f28f08401fb4
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-09 16:14:48 +0300

    Create fun loadSounds() in BeatBox

--- app/src/main/java/io/github/kirillmokretsov/beatbox/BeatBox.kt
+++ app/src/main/java/io/github/kirillmokretsov/beatbox/BeatBox.kt
@@ -1,8 +1,22 @@
 package io.github.kirillmokretsov.beatbox
 
 import android.content.res.AssetManager
+import android.util.Log
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
 
-class BeatBox (private val assets: AssetManager)
+class BeatBox(private val assets: AssetManager) {
+
+    fun loadSounds(): List<String> {
+        val soundNames = assets.list(SOUNDS_FOLDER)
+        return if (soundNames != null) {
+            Log.d(TAG, "Found ${soundNames.size} sounds")
+            soundNames.asList()
+        } else {
+            Log.e(TAG, "Could not list assets")
+            emptyList()
+        }
+    }
+
+}
