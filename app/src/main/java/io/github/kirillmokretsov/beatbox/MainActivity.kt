b718a7a14f23fc272cc8ccb4423fceb4befc4eac
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-09 15:31:59 +0300

    Create SoundHolder

--- app/src/main/java/io/github/kirillmokretsov/beatbox/MainActivity.kt
+++ app/src/main/java/io/github/kirillmokretsov/beatbox/MainActivity.kt
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.github.kirillmokretsov.beatbox.databinding.ActivityMainBinding
+import io.github.kirillmokretsov.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityMainBinding =
-                DataBindingUtil.setContentView(this, R.layout.activity_main)
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
         }
     }
+
+    private inner class SoundHolder(private val binding: ListItemSoundBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
