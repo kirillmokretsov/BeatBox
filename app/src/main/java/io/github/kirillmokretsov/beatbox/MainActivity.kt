3db19382b01f13aaf871c5161db90d37f466712a
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-09 15:27:34 +0300

    Inflate a binding class in MainActivity.kt

--- app/src/main/java/io/github/kirillmokretsov/beatbox/MainActivity.kt
+++ app/src/main/java/io/github/kirillmokretsov/beatbox/MainActivity.kt
@@ -2,10 +2,14 @@ package io.github.kirillmokretsov.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import io.github.kirillmokretsov.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
