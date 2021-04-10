86ce29e4de59e6a7c305c2d864a9e9b42c14d362
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-10 12:30:50 +0300

    Add a sound id propery

--- app/src/main/java/io/github/kirillmokretsov/beatbox/Sound.kt
+++ app/src/main/java/io/github/kirillmokretsov/beatbox/Sound.kt
@@ -2,7 +2,7 @@ package io.github.kirillmokretsov.beatbox
 
 private const val WAV = ".wav"
 
-class Sound(assetPath: String) {
+class Sound(assetPath: String, var soundId: Int? = null) {
 
     val name = assetPath.split("/").last().removeSuffix(WAV)
 
