732fcd28893caa7033a01c2f9ec22229941ba621
Kirill-Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-10 13:53:03 +0300

    Release soundsPool when BeatBoxViewModel.kt is clearing

--- app/src/main/java/io/github/kirillmokretsov/beatbox/BeatBoxViewModel.kt
+++ app/src/main/java/io/github/kirillmokretsov/beatbox/BeatBoxViewModel.kt
@@ -4,4 +4,10 @@ import androidx.lifecycle.ViewModel
 
 class BeatBoxViewModel() : ViewModel() {
     lateinit var beatBox: BeatBox
+
+    override fun onCleared() {
+        super.onCleared()
+        beatBox.release()
+    }
+
 }
